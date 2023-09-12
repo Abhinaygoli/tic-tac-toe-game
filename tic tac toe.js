ca6a4873d5e3281560f43a3b35ac90c3c01bbaf2
Abhinaygoli <143925505+Abhinaygoli@users.noreply.github.com>
2023-09-12 13:17:45 +0530

    Update tic tac toe.js

--- tic tac toe.js
+++ tic tac toe.js
@@ -1,18 +1,30 @@
 var sign = "X";
 
-var playerDetail = document.getElementById("player")
+var winningMessageElement = document.getElementById("winningMessage")
+var playerDetail1 = document.getElementById("player1")
+var playerDetail2 = document.getElementById("player2")
+var winnerMessageElement = document.getElementById("winner-message")
+var congratsMessageElement = document.getElementById("congrats-message")
+
 function boxClicked(id){
     var clickedElement = document.getElementById(id);
     if(clickedElement.innerText == ""){
         clickedElement.innerText = sign;
+        player1.style.color = "red"
         winner(sign)
         if(sign == "X"){
             sign = "O"
-            player.innerText = "Player 2"
+            player1.style.color = "black"
+            player1.style.backgroundColor = "bisque"
+            player2.style.backgroundColor = "white"
+            player2.style.color = "red"
             clickedElement.style.backgroundColor = "yellow"
         }else{
             sign = "X"
-            player.innerText = "Player 1"
+            player2.style.color = "black"
+            player2.style.backgroundColor = "bisque"
+            player1.style.backgroundColor = "white"
+            player1.style.color = "red"
             clickedElement.style.backgroundColor = "orange"
         }
     }
@@ -28,7 +40,8 @@ function winner(sign){
     || document.getElementById("box3").innerHTML == sign && document.getElementById("box6").innerHTML == sign && document.getElementById("box9").innerHTML == sign){
         if(sign == "X"){
             setTimeout(function(){
-                alert("Player 1 is winner")
+                winningMessageElement.style.display = "block"
+                winnerMessageElement.innerText = "Player 1,You are the Winner"
             },200)
             setTimeout(function(){
                 location.reload()
@@ -36,7 +49,8 @@ function winner(sign){
             
         }else{   
             setTimeout(function(){
-                alert("Player 2 is winner")
+                winningMessageElement.style.display = "block"
+                winnerMessageElement.innerText = "Player 2,You are the Winner"
             },200)
             setTimeout(function(){
                 location.reload()
@@ -45,7 +59,14 @@ function winner(sign){
     }else if(document.getElementById("box1").innerText != "" && document.getElementById("box2").innerText != "" && document.getElementById("box3").innerText != "" &&
     document.getElementById("box4").innerText != ""&& document.getElementById("box5").innerText != "" && document.getElementById("box6").innerText != "" &&
     document.getElementById("box7").innerText != "" && document.getElementById("box8").innerText != "" && document.getElementById("box9").innerText != ""){
-        alert("Neither Player 1 nor Player 2 is the Winner, Please restart the game!!")
-        location.reload()
+        setTimeout(function(){
+            winningMessageElement.style.display = "block"
+            congratsMessageElement.innerText = ""
+            winnerMessageElement.innerText = "Tie, Please restart the game!!"
+        },200)
+        setTimeout(function(){
+            location.reload()
+        },3000)
+        
     }
 }
